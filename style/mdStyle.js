8c106e82667d03803c58300bde84e6813d054e76
Peter Riemenschneider <peter@vmi1199568.contaboserver.net>
2025-01-25 18:24:11 +0100

    add style

---
+++ style/mdStyle.js
@@ -0,0 +1,15 @@
+let mdStyle = {
+	h1: {fontSize: '2rem', margin: '0 1rem',},
+	h2: {fontSize: '1.5rem'},
+	h3: {fontSize: '1.2rem'},
+	h4: {fontSize: '1rem'},
+	h5: {fontSize: '1rem'},
+	h6: {fontSize: '1rem'},
+	p: {margin: '1rem 0'},
+	code: {fontFamily: 'monospace'},
+	block: {margin: '0px 40px', color: 'purple', backgroundColor: 'lightgrey',},
+	a: {color: 'blue', textDecoration: 'underline',},
+	ul: {margin: '0 0 0 10px'},
+	ol: {margin: '0 0 0 10px'},
+	li: {listStylePosition: 'outside', margin: '0 0 0 30px'},
+};
